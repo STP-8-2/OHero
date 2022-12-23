6136d890e05dd524ba66d5acebb4648018e15800
Troy2797B <112650105+Troy2797B@users.noreply.github.com>
2022-12-22 22:03:29 -0500

    Add files via upload

---
+++ SaveThePrincess/src/test/java/rocks/zipcode/SaveThePrincess/SaveThePrincessApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.SaveThePrincess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveThePrincessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
